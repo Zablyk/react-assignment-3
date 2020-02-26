19f61f25ad5fb84d4750b6e944d3b32f2e0f59ba
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-02-26 10:58:50 -0500

    added a link in to Courses.js to make a course clickable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Courses from './containers/Courses/Courses';
+import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
 import { Route, NavLink, Switch } from 'react-router-dom';
 
@@ -9,20 +10,6 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <header>
-          <nav>
-            <ul style={{listStyle: 'none', margin: 'auto', padding: '0'}}>
-              <li style={{margin: '10px', display: 'inline-block'}}><NavLink to="/users">Users</ NavLink></li>
-              <li><NavLink to="/courses">Courses</ NavLink></li>
-            </ul>
-          </nav>
-        </header>
-        <Switch>
-            <Route path="/users"
-                   component={Users} />
-            <Route path="/courses"
-                   component={Courses} />
-        </Switch>
         <ol style={{textAlign: 'left'}}>
           <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
           <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
@@ -33,6 +20,18 @@ class App extends Component {
           <li>Add a 404 error page and render it for any unknown routes</li>
           <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
         </ol>
+
+        <nav>
+            <ul style={{listStyle: 'none', margin: 'auto', padding: '0'}}>
+              <li style={{margin: '10px', display: 'inline-block'}}><NavLink to="/users">Users</ NavLink></li>
+              <li style={{margin: '10px', display: 'inline-block'}}><NavLink to="/courses">Courses</ NavLink></li>
+            </ul>
+        </nav>
+        <Switch>
+          <Route path="/users" component={Users} />
+          <Route path="/courses/course" component={Course} />
+          <Route path="/courses" component={Courses} />
+        </Switch>
       </div>
     );
   }
