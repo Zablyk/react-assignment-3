754270e38a71f8ce2cb1c367d567111b2b442ab8
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-02-27 11:40:06 -0500

    added redirect from all-courses to courses

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Courses from './containers/Courses/Courses';
 import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
 import NoMatch from './components/NoMatch/NoMatch';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import noMAtch from './components/NoMatch/NoMatch';
 
 
@@ -33,6 +33,7 @@ class App extends Component {
           <Route path="/users" component={Users} />
           {/* <Route path="/courses/:courseId" component={Course} /> */}
           <Route path="/courses" component={Courses} />
+          <Redirect from="/all-courses" to="/courses" />
           <Route component={NoMatch}/>
         </Switch>
       </div>
